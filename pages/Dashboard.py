738003befb30065d8ce7d6969ef27b8869daa8e1
AndreDXDS <andre.dharmalingam@xds.co.za>
2025-01-14 13:51:17 +0200

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -19,5 +19,5 @@ st.title("XDS Universe")
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.metric(label="Universe", value="58 M")
+    st.metric(label="Universe", value="58 M", border=True)
 
