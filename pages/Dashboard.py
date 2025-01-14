46b51a64cec6712fd1c839f7aaecc422ccadc33b
AndreDXDS <andre.dharmalingam@xds.co.za>
2025-01-14 13:25:34 +0200

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -2,16 +2,14 @@ import streamlit as st
 import Modules as md
 
 # Inject custom CSS for wide buttons
-def set_css():
-    st.markdown(
-        """
-        <style>
-        .stButton > button {
-            width: 100%;
-            margin-bottom: 10px;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True,
-    )
+st.markdown(
+    """
+    <style>
+    [data-testid="stSidebar"]{
+        visibility: hidden;
+    }
+    </style>
+    """, 
+    unsafe_allow_html=True
+)
 
