2dbc591ae3cb46f6c0402ec52ebef3b0ff356f14
AndreDXDS <andre.dharmalingam@xds.co.za>
2025-01-14 13:51:51 +0200

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -20,4 +20,7 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
     st.metric(label="Universe", value="58 M", border=True)
-
+with col2:
+    st.metric(label="Universe", value="58 M", border=True)
+with col3:
+    st.metric(label="Universe", value="58 M", border=True)
