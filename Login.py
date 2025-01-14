0300e30c6a095f057c7899e527459d740b7c78bd
AndreDXDS <andre.dharmalingam@xds.co.za>
2025-01-14 13:05:57 +0200

    Update Login.py

--- Login.py
+++ Login.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import Modules as md
 
 # Inject custom CSS for wide buttons
 def set_css():
@@ -13,17 +14,5 @@ def set_css():
         """,
         unsafe_allow_html=True,
     )
-    
-def login():
-    st.title("Login")
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
 
-    if st.button("Login"):
-        if username == "admin" and password == "password":
-            st.session_state["authenticated"] = True
-            st.success("Login successful!")
-        else:
-            st.error("Invalid username or password")
-
-login()
+md.login()
