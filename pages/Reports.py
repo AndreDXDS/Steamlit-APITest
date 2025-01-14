a974a62791d7c8b7bfd5f4eaedbd61725f931982
AndreDXDS <andre.dharmalingam@xds.co.za>
2025-01-14 15:10:37 +0200

    Update Reports.py

--- pages/Reports.py
+++ pages/Reports.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import Modules as md
 
+st.markdown(
+    """
+    <style>
+    [data-testid="stSidebar"]{
+        visibility: hidden;
+    }
+    </style>
+    """, 
+    unsafe_allow_html=True
+)
