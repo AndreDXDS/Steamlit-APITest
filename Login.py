bf029d4c23dfd9fe908c59b5fdcc1906c6854ab5
AndreDXDS <andre.dharmalingam@xds.co.za>
2025-01-14 13:25:05 +0200

    Update Login.py

--- Login.py
+++ Login.py
@@ -1,18 +1,15 @@
 import streamlit as st
 import Modules as md
 
-# Inject custom CSS for wide buttons
-def set_css():
-    st.markdown(
-        """
-        <style>
-        .stButton > button {
-            width: 100%;
-            margin-bottom: 10px;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True,
-    )
+st.markdown(
+    """
+    <style>
+    [data-testid="stSidebar"]{
+        visibility: hidden;
+    }
+    </style>
+    """, 
+    unsafe_allow_html=True
+)
 
 md.login()
