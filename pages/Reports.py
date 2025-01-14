a8f3ec8d8dea6e2a7b5f4e6a4a26dca546bf9a9c
AndreDXDS <andre.dharmalingam@xds.co.za>
2025-01-14 15:13:35 +0200

    Update Reports.py

--- pages/Reports.py
+++ pages/Reports.py
@@ -13,3 +13,17 @@ st.markdown(
     """, 
     unsafe_allow_html=True
 )
+
+col1, col2 = st.columns([1,4])
+
+with col1:
+    st.title("Reports")
+    
+    if st.button("Enquiries"):
+        st.switch_page("pages/Enquiries.py")
+with col2:
+    st.write("")
+    st.write("")
+    st.write("")
+    if st.button("Reports", use_container_width=True):
+        st.switch_page("pages/Reports.py")
